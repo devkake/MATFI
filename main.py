0e20b42f0a07c564dbc75b430cb5a5077e867add
devkake <sofujikake.dev@gmail.com>
2016-06-03 14:33:54 +0200

    so:fist commit

---
+++ main.py
@@ -0,0 +1,5 @@
+global subjectExplication
+subjectExplication = "Calculation of Price for max[0,T](St-K)+. Evaluation of convergence speed in proportion to step"
+
+if __name__ == '__main__':
+	print(subjectExplication)
